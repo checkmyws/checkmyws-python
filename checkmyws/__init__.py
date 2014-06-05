4246b042ee880505fe61484c6fbfd1a473f301d7
William Pain <pain.william@gmail.com>
2014-06-05 17:15:06 +0200

    Bump version v0.1.1

--- checkmyws/__init__.py
+++ checkmyws/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Check my Website Team"
 __author_email__ = "contact@checkmy.ws"
 __copyright__ = "Copyright 2014 Novateek"
 __license__ = "MIT License"
-__version__ = "0.1"
+__version__ = "0.1.1"
 __url__ = "http://www.checkmy.ws/"
 
 from checkmyws.client import CheckmywsClient  # NOQA
