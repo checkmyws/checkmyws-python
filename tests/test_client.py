99fe7d9ac7e55a21a7192ca9073b9bace82ba1b5
William Pain <pain.william@gmail.com>
2014-10-13 14:26:53 +0200

    Replace mock by httmock

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,55 +10,45 @@ import json
 import unittest
 import requests
 
-from mock import patch
+from httmock import all_requests, HTTMock
 
 from checkmyws import CheckmywsClient
 from checkmyws import CheckmywsError
 
 client = None
+check_id = "9597dc65-6a94-4a91-8200-63b1368a7e1c"
+
+@all_requests
+def mock_200(url, request):
+    return {'status_code': 200, 'content': None}
 
 
-def _build_response_object(status_code=200, content=""):
-    resp = requests.Response()
-    resp.status_code = status_code
-    resp._content = content
-    return resp
+@all_requests
+def mock_404(url, request):
+    return {'status_code': 404, 'content': None}
 
 
-def _mocked_session(method="GET", status_code=200, content=""):
+@all_requests
+def mock_401(url, request):
+    return {'status_code': 401, 'content': None}
 
-    method = method.upper()
 
-    def request(*args, **kwargs):
-        c = content
-
-        # Check method
-        assert method == kwargs.get('method', 'GET')
-
-        # Check requests options
-        if method == 'POST':
-            data = kwargs.get('data', None)
-
-            if data is not None:
-                # Data must be a string
-                assert isinstance(data, str)
-
-                # Data must be a JSON string
-                json.loads(data)
-
-        # Anyway, Content must be a JSON string (or empty string)
-        if not isinstance(c, str):
-            c =  json.dumps(c).encode('utf8')
-
-        return _build_response_object(status_code=status_code, content=c)
-
-    mocked = patch.object(
-        client.session,
-        'request',
-        side_effect = request
-        )
-
-    return mocked
+@all_requests
+def mock_status(url, request):
+    status = {
+        "_id": check_id,
+        "laststatechange_bin": 1392506884,
+        "metas": {
+            "title": "Check my Website",
+        },
+        "state": 0,
+        "states": {
+            "ES:MAD:OVH:DC": 0,
+            "US:NY:DGO:DC": 0
+        },
+        "url": "http://www.checkmy.ws"
+    }
+    return {'status_code': 200, 'content': status}
 
 
 class TestCase(unittest.TestCase):
@@ -68,56 +58,50 @@ class TestCase(unittest.TestCase):
         client = CheckmywsClient()
 
     def test_02_request(self):
-        with _mocked_session('get', 200) as mocked:
+        with HTTMock(mock_200):
             client.request("/one/path", method="GET", status_code=200)
 
-        with _mocked_session('post', 200) as mocked:
+        with HTTMock(mock_200):
             client.request("/one/path", method="POST", status_code=200)
 
-        with _mocked_session('get', 200) as mocked:
+        with HTTMock(mock_200):
             client.request(
                 "/one/path", method="GET", params={'key': 'value'}, status_code=200
             )
 
-        with _mocked_session('post', 200) as mocked:
+        with HTTMock(mock_200):
             client.request(
                 "/one/path", method="POST", data={'key': 'value'}, status_code=200
             )
 
-        with _mocked_session('get', 404) as mocked:
+        with HTTMock(mock_404):
             client.request("/one/path", method="GET", status_code=404)
 
-        with _mocked_session('get', 404) as mocked:
+        with HTTMock(mock_404):
             with self.assertRaises(CheckmywsError):
                 client.request("/one/path", method="GET", status_code=200)
 
-        with _mocked_session('get', 404) as mocked:
+        with HTTMock(mock_404):
             with self.assertRaises(CheckmywsError):
                 client.request("/one/path", method="GET", status_code=200)
+
+        client_proxy = CheckmywsClient(proxy="http://127.0.0.1")
+        with HTTMock(mock_200):
+            client_proxy.request("/one/path", method="GET", status_code=200)
 
     def test_03_status(self):
-        check_id = "3887e18a-28d6-4eac-9eb0-c6d9075e4c7e"
-        status = {
-            "_id": check_id,
-            "laststatechange_bin": 1392506884,
-            "metas": {
-                "title": "Check my Website",
-            },
-            "state": 0,
-            "states": {
-                "ES:MAD:OVH:DC": 0,
-                "US:NY:DGO:DC": 0
-            },
-            "url": "http://www.checkmy.ws"
-        }
-
-        with _mocked_session('get', 401) as mocked:
+        with HTTMock(mock_401):
             with self.assertRaises(CheckmywsError):
                 client.status('mycheckid')
 
-        with _mocked_session('get', 200, status) as mocked:
+        with HTTMock(mock_status):
             data = client.status(check_id)
             self.assertEqual(data['_id'], check_id)
+
+        # Real check
+        data = client.status(check_id)
+        self.assertEqual(data['_id'], check_id)
+        print("URL: %s" % data['url'])
 
 if __name__ == '__main__':
     unittest.main()
